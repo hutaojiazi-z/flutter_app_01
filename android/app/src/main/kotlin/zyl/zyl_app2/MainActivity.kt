f46c34f87e430b252440983e280fcdf74602c73b
zengyaling@maytek.cn <zengyaling@maytek.cn>
2021-07-28 15:03:13 +0800

    补充第一次提交

---
+++ android/app/src/main/kotlin/zyl/zyl_app2/MainActivity.kt
@@ -0,0 +1,6 @@
+package zyl.zyl_app2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/viewmodel/account_viewmodel.dart
+++ lib/viewmodel/account_viewmodel.dart
@@ -5,6 +5,8 @@ import 'package:zyl_app2/model/account_model.dart';
 class AccountViewModel extends ChangeNotifier {
   List _list = [];
   int _month;
+  double _expenditure = 0; //支出
+  double _income = 0; //收入
 
   List get getList {
     return _list;
@@ -24,7 +26,25 @@ class AccountViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  void getAccountHistory() async {
+  double get getExpenditure {
+    return _expenditure;
+  }
+
+  void setExpenditure(double val) {
+    _expenditure = val;
+    notifyListeners();
+  }
+
+  double get getIncome {
+    return _income;
+  }
+
+  void setIncome(double val) {
+    _income = val;
+    notifyListeners();
+  }
+
+  void accountingHistory() async {
     Response result = await getAccount(_month.toString());
     print(result);
   }
